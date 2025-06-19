f6c8643bd591a3b701fe8b7b4cfbf71f226d6cc9
pat56-hp <patrickkouassi7@gmail.com>
2025-06-19 16:25:13 +0000

    Analytic: Ajout du package Analytic de vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
